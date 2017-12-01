3c216e9d234a558f8bd88987a7fac41585485278
Анциферов Ян <Anci_oq88>
2017-12-01 17:47:29 +0200

    first commit

---
+++ TreeView/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace TreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Tree = new System.Windows.Forms.TreeView();
+            this.List = new System.Windows.Forms.ListView();
+            this.Path = new System.Windows.Forms.TextBox();
+            this.imageListSmall = new System.Windows.Forms.ImageList(this.components);
+            this.imageListLarge = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // Tree
+            // 
+            this.Tree.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Tree.ImageIndex = 0;
+            this.Tree.ImageList = this.imageListSmall;
+            this.Tree.Location = new System.Drawing.Point(1, 2);
+            this.Tree.Name = "Tree";
+            this.Tree.SelectedImageIndex = 0;
+            this.Tree.Size = new System.Drawing.Size(245, 530);
+            this.Tree.TabIndex = 0;
+            // 
+            // List
+            // 
+            this.List.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.List.LargeImageList = this.imageListLarge;
+            this.List.Location = new System.Drawing.Point(252, 28);
+            this.List.Name = "List";
+            this.List.Size = new System.Drawing.Size(736, 504);
+            this.List.SmallImageList = this.imageListSmall;
+            this.List.TabIndex = 1;
+            this.List.UseCompatibleStateImageBehavior = false;
+            // 
+            // Path
+            // 
+            this.Path.Location = new System.Drawing.Point(252, 2);
+            this.Path.Name = "Path";
+            this.Path.Size = new System.Drawing.Size(736, 20);
+            this.Path.TabIndex = 2;
+            // 
+            // imageListSmall
+            // 
+            this.imageListSmall.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListSmall.ImageStream")));
+            this.imageListSmall.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListSmall.Images.SetKeyName(0, "file.png");
+            this.imageListSmall.Images.SetKeyName(1, "folder.png");
+            this.imageListSmall.Images.SetKeyName(2, "hard-drive.png");
+            this.imageListSmall.Images.SetKeyName(3, "laptop.png");
+            // 
+            // imageListLarge
+            // 
+            this.imageListLarge.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListLarge.ImageStream")));
+            this.imageListLarge.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListLarge.Images.SetKeyName(0, "file (1).png");
+            this.imageListLarge.Images.SetKeyName(1, "folder (1).png");
+            this.imageListLarge.Images.SetKeyName(2, "hard-drive (1).png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 532);
+            this.Controls.Add(this.Path);
+            this.Controls.Add(this.List);
+            this.Controls.Add(this.Tree);
+            this.Name = "Form1";
+            this.Text = "Проводник";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView Tree;
+        private System.Windows.Forms.ListView List;
+        private System.Windows.Forms.TextBox Path;
+        private System.Windows.Forms.ImageList imageListSmall;
+        private System.Windows.Forms.ImageList imageListLarge;
+    }
+}
+
---
+++ TreeView/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace TreeView
+{
+    public partial class Form1 : Form
+    {
+        TreeNode mainNode;
+        public Form1()
+        {
+            InitializeComponent();
+            mainNode = new TreeNode("Мой компьютер");
+            mainNode.Name = "";
+            mainNode.Nodes.Add("");
+            mainNode.ImageIndex = 2;
+            Tree.Nodes.Add(mainNode);
+            Tree.BeforeExpand += Tree_BeforeExpand;
+            Tree.BeforeSelect += Tree_BeforeSelect;
+            List.DoubleClick += List_DoubleClick;
+        }
+
+        private void List_DoubleClick(object sender, EventArgs e)
+        {
+            if (List.SelectedItems.Count == 1)
+            {
+                
+                if(Path.Text == "Мой компьютер")
+                {
+                    string newPath = List.SelectedItems[0].Text + "\\";
+
+                    var dirs = new DirectoryInfo(newPath).GetDirectories();
+                    List.Items.Clear();
+                    foreach (var item in dirs)
+                    {
+                        ListViewItem newNode = new ListViewItem(item.Name);
+                        newNode.ImageIndex = 1;
+                        List.Items.Add(newNode);
+                    }
+
+                    foreach (var item in new DirectoryInfo(newPath).GetFiles())
+                    {
+                        ListViewItem newNode = new ListViewItem(item.Name);
+                        newNode.Name = item.Name;
+                        newNode.ImageIndex = 0;
+                        List.Items.Add(newNode);
+                    }
+                    Path.Text = newPath;
+                }
+                else
+                {
+
+                    string newPath =  Path.Text + List.SelectedItems[0].Text + "\\";
+                
+                    var Node = List.SelectedItems[0];
+
+                    try
+                    {
+                        var dirs = new DirectoryInfo(newPath).GetDirectories();
+                        List.Items.Clear();
+                        foreach (var item in dirs)
+                        {
+                            ListViewItem newNode = new ListViewItem(item.Name);
+                            newNode.ImageIndex = 1;
+                            List.Items.Add(newNode);
+                        }
+
+                        foreach (var item in new DirectoryInfo(newPath).GetFiles())
+                        {
+                            ListViewItem newNode = new ListViewItem(item.Name);
+                            newNode.Name = item.Name;
+                            newNode.ImageIndex = 0;
+                            List.Items.Add(newNode);
+                        }
+                        Path.Text = newPath;
+
+                    }
+                    catch (IOException ex)
+                    {
+                    
+                    }
+                }
+
+            }
+        }
+
+       
+
+        void Tree_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                List.Items.Clear();
+                if (e.Node.FullPath == "Мой компьютер")
+                {
+                    Path.Text = "Мой компьютер";
+                    foreach (var item in DriveInfo.GetDrives())
+                    {
+                        ListViewItem newNode = new ListViewItem(item.Name);
+                        Tree.BeforeSelect -= Tree_BeforeSelect;
+                        Tree.SelectedImageIndex = 3;
+                        Tree.BeforeSelect += Tree_BeforeSelect;
+                        newNode.ImageIndex = 2;
+                        List.Items.Add(newNode);
+                    }
+                }
+                else
+                {
+                   
+                    Path.Text = GetParrentName(e.Node);
+                    foreach (var item in new DirectoryInfo(GetParrentName(e.Node)).GetDirectories())
+                    {
+                        ListViewItem newNode = new ListViewItem(item.Name);
+                        
+                        newNode.ImageIndex = 1;
+                        List.Items.Add(newNode);
+                    }
+
+                    foreach (var item in new DirectoryInfo(GetParrentName(e.Node)).GetFiles())
+                    {
+                        ListViewItem newNode = new ListViewItem(item.Name);
+                        
+                        newNode.ImageIndex = 0;
+                        newNode.Name = item.Name;
+                        List.Items.Add(newNode);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Tree.BeforeSelect -= Tree_BeforeSelect;
+                Tree.SelectedImageIndex = 2;
+                Tree.BeforeSelect += Tree_BeforeSelect;   
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+     
+
+        private void Tree_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                e.Node.Nodes.Clear();
+                if (e.Node.FullPath == "Мой компьютер")
+                {
+                    e.Node.ImageIndex = 3;
+                    foreach (var item in DriveInfo.GetDrives())
+                    {
+                        TreeNode newNode = new TreeNode(item.Name);
+                        newNode.Nodes.Add("");
+                        newNode.Name = item.Name;
+                        newNode.ImageIndex = 2;
+                        e.Node.Nodes.Add(newNode);
+                    }
+                }
+                else
+                {
+                    //MessageBox.Show(GetParrentName(e.Node));
+                    foreach (var item in new DirectoryInfo(GetParrentName(e.Node)).GetDirectories())
+                    {
+                        TreeNode newNode = new TreeNode(item.Name);
+                        newNode.Nodes.Add("");
+                        newNode.ImageIndex = 1;
+                        newNode.Name = item.Name;
+                        e.Node.Nodes.Add(newNode);
+                    }
+
+                    foreach (var item in new DirectoryInfo(GetParrentName(e.Node)).GetFiles())
+                    {
+                        TreeNode newNode = new TreeNode(item.Name);
+                        newNode.ImageIndex = 0;
+                        newNode.Name = item.Name;
+                        e.Node.Nodes.Add(newNode);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+
+
+        }
+
+        string GetParrentName(TreeNode node)
+        {
+            if (node.Parent != null)
+                return  GetParrentName(node.Parent) + node.Name + "\\";
+            else return "";
+        }
+    }
+}
+
